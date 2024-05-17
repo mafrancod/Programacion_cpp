8c77c551b5f9a93e915c068e8008814f85eaf24d
Mariajo <mafrancod@unal.edu.co>
2024-05-17 11:36:08 -0500

    Subiendo la carpeta programaciòn

---
+++ programacion-cpp/hola-mundo.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+std:: cout<< "Hola mundo" << std::endl; // Imprime "Hola Mundo" y un salto de línea
+    return 0; // Retorna 0 indicando que el programa finalizó correctamente
+
+}
